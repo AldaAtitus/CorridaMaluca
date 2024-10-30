82ac0658a0453ca298f314b88c5a7d5b66b7d214
Aldacir Stanguerlin Junior <1137226@atitus.edu.br>
2024-10-30 19:15:13 -0300

    "Mudanças Finais"

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ movXcar1 = 0
 movXcar2 = 0
 posYcar1 = 50
 posYcar2 = 180
+vitoria = pygame.mixer.Sound("assets/vitoria.mp3")
+vitoria.set_volume(0.5)
 pygame.mixer.music.load("assets/trilha.mp3")
 pygame.mixer.music.play(-1) #looping
+acabou = False
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -25,17 +28,34 @@ while True:
     tela.blit(fundo, (0,0))
     tela.blit(carro1, (movXcar1,posYcar1))
     tela.blit(carro2, (movXcar2,posYcar2))
-    movXcar1 = movXcar1 + random.randint(0,5)
-    movXcar2 = movXcar2 + random.randint(0,5)
     
-
+    if not acabou:
+        movXcar1 = movXcar1 + random.randint(0,10)
+        movXcar2 = movXcar2 + random.randint(0,10)
     
     if movXcar1 > 1000:
         movXcar1 = 0
         posYcar1 = 350
+    
     if movXcar2 > 1000:
         movXcar2 = 0
         posYcar2 = 480
+        
+    fonte = pygame.font.Font("freesansbold.ttf", 60) #ttf é a fonte
+    textoVermelho = fonte.render("Vermelho Ganhou!!!", True, branco)
+    textoAmarelo = fonte.render("Amarelo Ganhou!", True, branco)
+    
+    if posYcar1 == 350 and movXcar1 >= 900 and movXcar1>movXcar2:
+        tela.blit(textoVermelho, (270,70))
+        acabou = True
+        pygame.mixer.music.stop()
+        pygame.mixer.Sound.play(vitoria)
+        
+    elif posYCar2 == 480 and movXCar2 >= 900 and movXCar2 > movXCar1:
+        tela.blit(textoAmarelo, (270,180))
+        acabou = True
+        pygame.mixer.music.stop()
+        pygame.mixer.Sound.play(vitoria)
     
     pygame.display.update()
     clock.tick(60)
