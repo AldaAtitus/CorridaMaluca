3624a3e02ec37d8ef69f5c2e16e58d1e681f500a
Aldacir Stanguerlin Junior <1137226@atitus.edu.br>
2024-11-05 21:58:31 -0300

    Adição do carro 3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
+from os import system
 pygame.init()
+system("cls")
 tamanho = (1000,592)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)
@@ -12,10 +14,13 @@ preto = (0,0,0)
 fundo = pygame.image.load("recursos/fundo.png")
 carro1 = pygame.image.load("recursos/carro1.png")
 carro2 = pygame.image.load("recursos/carro2.png")
+carro3 = pygame.image.load("recursos/carro3.png")
 movXcar1 = 0
 movXcar2 = 0
+movXcar3 = 0
 posYcar1 = 50
 posYcar2 = 180
+posYcar3 = 110
 vitoria = pygame.mixer.Sound("recursos/vitoria.mp3")
 vitoria.set_volume(0.5)
 pygame.mixer.music.load("recursos/trilha.mp3")
@@ -31,10 +36,12 @@ while True:
     tela.blit(fundo, (0,0))
     tela.blit(carro1, (movXcar1,posYcar1))
     tela.blit(carro2, (movXcar2,posYcar2))
+    tela.blit(carro3, (movXcar3,posYcar3))
     
     if not acabou:
-        movXcar1 = movXcar1 + random.randint(0,10)
-        movXcar2 = movXcar2 + random.randint(0,10)
+        movXcar1 = movXcar1 + random.randint(0,12)
+        movXcar2 = movXcar2 + random.randint(0,12)
+        movXcar3 = movXcar3 + random.randint(0,12)
     else:
         pygame.mixer.music.stop()
         if somDaVitoria == False:
@@ -48,20 +55,27 @@ while True:
     if movXcar2 > 1000:
         movXcar2 = 0
         posYcar2 = 480
+    
+    if movXcar3 > 1000:
+        movXcar3 = 0
+        posYcar3 = 410
         
     fonte = pygame.font.Font("freesansbold.ttf", 60) #ttf é a fonte
     textoVermelho = fonte.render("Vermelho Ganhou!!!", True, branco)
     textoAmarelo = fonte.render("Amarelo Ganhou!", True, branco)
+    textoAzul = fonte.render("Azul Ganhou!", True, branco)
     
-    if posYcar1 == 350 and movXcar1 >= 900 and movXcar1>movXcar2:
+    if posYcar1 == 350 and movXcar1 >= 900 and movXcar1> movXcar2 and movXcar3:
         tela.blit(textoVermelho, (270,70))
         acabou = True
-
-        
-    elif posYcar2 == 480 and movXcar2 >= 900 and movXcar2 > movXcar1:
-        tela.blit(textoAmarelo, (270,180))
+    
+    elif posYcar2 == 480 and movXcar2 >= 900 and movXcar2 > movXcar1 and movXcar3:
+        tela.blit(textoAmarelo, (270,70))
         acabou = True
-
+    
+    elif posYcar3 == 410 and movXcar3 >= 900 and movXcar3 > movXcar1 and  movXcar2:
+        tela.blit(textoAmarelo, (270,70))
+        acabou = True
     
     pygame.display.update()
     clock.tick(60)
