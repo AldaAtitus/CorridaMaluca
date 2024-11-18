f9e810df853375898e589de74b84bcf165805199
Aldacir Stanguerlin Junior <1137226@atitus.edu.br>
2024-11-17 21:46:22 -0300

    Adicionado Fundo da Vitória

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ pygame.init()
 system("cls")
 tamanho = (1000,592)
 clock = pygame.time.Clock()
+tamanho = (896,597)
 tela = pygame.display.set_mode(tamanho)
 icone = pygame.image.load("recursos/icone.ico")
 pygame.display.set_icon(icone)
 pygame.display.set_caption("Corrida Maluca")
 branco = (255,255,255)
 preto = (0,0,0)
+vermelho = (255, 0, 0) 
+azul = (0, 0, 255) 
+amarelo = (255, 255, 0)
 fundo = pygame.image.load("recursos/fundo.png")
+finalCorrida = pygame.image.load("recursos/finalCorrida.png")
 carro1 = pygame.image.load("recursos/carro1.png")
 carro2 = pygame.image.load("recursos/carro2.png")
 carro3 = pygame.image.load("recursos/carro3.png")
@@ -33,7 +38,7 @@ while True:
             quit()
     
     tela.fill(branco)
-    tela.blit(fundo, (0,0))
+    tela.blit(fundo, (0, 0))
     tela.blit(carro1, (movXcar1,posYcar1))
     tela.blit(carro2, (movXcar2,posYcar2))
     tela.blit(carro3, (movXcar3,posYcar3))
@@ -47,7 +52,7 @@ while True:
         if somDaVitoria == False:
             pygame.mixer.Sound.play(vitoria)
             somDaVitoria = True
-    
+
     if movXcar1 > 1000:
         movXcar1 = 0
         posYcar1 = 350
@@ -61,10 +66,14 @@ while True:
         posYcar3 = 410
         
     fonte = pygame.font.Font("freesansbold.ttf", 60) #ttf é a fonte
-    textoVermelho = fonte.render("Vermelho Ganhou!!!", True, branco)
-    textoAmarelo = fonte.render("Amarelo Ganhou!", True, branco)
-    textoAzul = fonte.render("Azul Ganhou!", True, branco)
+    textoVermelho = fonte.render("Vermelho Ganhou!!!", True, vermelho)
+    textoAmarelo = fonte.render("Amarelo Ganhou!", True, amarelo)
+    textoAzul = fonte.render("Azul Ganhou!", True, azul)
     
+    if acabou == True:
+        tela.fill(branco)
+        tela.blit(finalCorrida, (0, 0))
+        
     if posYcar1 == 350 and movXcar1 >= 900 and movXcar1> movXcar2 and movXcar3:
         tela.blit(textoVermelho, (270,70))
         acabou = True
@@ -74,9 +83,9 @@ while True:
         acabou = True
     
     elif posYcar3 == 410 and movXcar3 >= 900 and movXcar3 > movXcar1 and  movXcar2:
-        tela.blit(textoAmarelo, (270,70))
+        tela.blit(textoAzul, (270,70))
         acabou = True
-    
+
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
